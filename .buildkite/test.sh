7dbcc934a1e89a1064bcda6f8cb891611bbfc278
Ryan Schlesinger <ryan@ryanschlesinger.com>
2020-11-02 11:46:04 -0800

    Fix hardcoded values

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -8,10 +8,21 @@ compose_params=(-f docker-compose.yml)
 if [[ -n "${BUILDKITE_PLUGIN_DOCKER_COMPOSE_OVERRIDE_FILE:-}" ]] ; then
   compose_params+=(-f "${BUILDKITE_PLUGIN_DOCKER_COMPOSE_OVERRIDE_FILE}")
 else
-  docker-compose "${compose_params[@]}" -p ${BUILDKITE_PLUGIN_DOCKER_COMPOSE_PROJECT_NAME} build --pull myservice
+  docker-compose \
+    "${compose_params[@]}" \
+    -p ${BUILDKITE_PLUGIN_DOCKER_COMPOSE_PROJECT_NAME} \
+    build --pull myservice
 fi
 
-docker-compose "${compose_params[@]}" -p ${BUILDKITE_PLUGIN_DOCKER_COMPOSE_PROJECT_NAME} up -d --scale myservice=0
-docker-compose "${compose_params[@]}" -p ${BUILDKITE_PLUGIN_DOCKER_COMPOSE_PROJECT_NAME} run \
-  --name ${BUILDKITE_PLUGIN_DOCKER_COMPOSE_CONTAINER_PREFIX}_myservice --rm \
+docker-compose \
+  "${compose_params[@]}" \
+  -p ${BUILDKITE_PLUGIN_DOCKER_COMPOSE_PROJECT_NAME} \
+  up -d --scale myservice=0
+
+docker-compose \
+  "${compose_params[@]}" \
+  -p ${BUILDKITE_PLUGIN_DOCKER_COMPOSE_PROJECT_NAME} \
+  run \
+  --name ${BUILDKITE_PLUGIN_DOCKER_COMPOSE_CONTAINER_PREFIX}_myservice \
+  --rm \
   myservice /bin/sh -e -c 'echo hello world'
--- tests/scripts/wrap_with_failure.sh
+++ tests/scripts/wrap_with_failure.sh
@@ -11,8 +11,8 @@ docker-compose \
 echo "--- Saying hello"
 docker-compose \
   -f tests/composefiles/docker-compose.v2.1.yml \
-  -p buildkitefbb18885a61849569d2d93b765659b63 \
-  --name buildkitefbb18885a61849569d2d93b765659b63_alpinewithfailinglink_build_11 \
+  -p ${BUILDKITE_PLUGIN_DOCKER_COMPOSE_PROJECT_NAME} \
+  --name ${BUILDKITE_PLUGIN_DOCKER_COMPOSE_CONTAINER_PREFIX}_alpinewithfailinglink \
   --rm \
   run \
   alpinewithfailinglink /bin/sh -e -c 'echo hello from alpine'
