eaf422615b629e6d2786b9a83bbb90978910d1c5
Ryan Schlesinger <ryan@ryanschlesinger.com>
2020-11-02 12:02:25 -0800

    Add prebuild spec

---
+++ tests/scripts/hello_world.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "--- Starting Dependencies"
+docker-compose \
+  -f tests/composefiles/docker-compose.v2.1.yml \
+  -p ${BUILDKITE_PLUGIN_DOCKER_COMPOSE_PROJECT_NAME} \
+  up -d --scale helloworldimage=0 \
+  helloworldimage
+
+echo "--- Saying hello"
+docker-compose \
+  -f tests/composefiles/docker-compose.v2.1.yml \
+  -p ${BUILDKITE_PLUGIN_DOCKER_COMPOSE_PROJECT_NAME} \
+  run \
+  --name ${BUILDKITE_PLUGIN_DOCKER_COMPOSE_CONTAINER_PREFIX}_helloworldimage \
+  --rm \
+  helloworldimage /hello
