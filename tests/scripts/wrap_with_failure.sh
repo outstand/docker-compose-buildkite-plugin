ef0919d203abdb47b54a1784b6ff6f1d3856c5e0
Ryan Schlesinger <ryan@ryanschlesinger.com>
2020-11-02 11:55:42 -0800

    Specify container under test

--- tests/scripts/wrap_with_failure.sh
+++ tests/scripts/wrap_with_failure.sh
@@ -6,7 +6,8 @@ echo "--- Starting Dependencies"
 docker-compose \
   -f tests/composefiles/docker-compose.v2.1.yml \
   -p ${BUILDKITE_PLUGIN_DOCKER_COMPOSE_PROJECT_NAME} \
-  up -d --scale alpinewithfailinglink=0
+  up -d --scale alpinewithfailinglink=0 \
+  alpinewithfailinglink 
 
 echo "--- Saying hello"
 docker-compose \
